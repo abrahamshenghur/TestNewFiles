2cbf8c51137aeed3b7e412f4d852560f3734a716
abrahamshenghur <ashenghur@gmail.com>
2021-12-17 15:33:30 -0800

    Initial commit

---
+++ TestNewFiles/TestNewFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewFilesApp.swift
+//  TestNewFiles
+//
+//  Created by Abraham on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TestNewFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
