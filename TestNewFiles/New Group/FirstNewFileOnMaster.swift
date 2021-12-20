4e6e228c06f7032db6ac6085e1145efbc0628c14
abrahamshenghur <ashenghur@gmail.com>
2021-12-19 21:38:49 -0800

    on master, did the upstream command

--- TestNewFiles/New Group/FirstNewFileOnMaster.swift
+++ TestNewFiles/New Group/FirstNewFileOnMaster.swift
@@ -15,3 +15,4 @@ import Foundation
 // On master will it push along with featuer-test?
 // will master get pushed while on feature-test branch?
 // will it work
+// on master, did the upstream command
